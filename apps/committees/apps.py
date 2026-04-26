4ce85622b280d49a7d419d35b2831a3e35e18a11
Cloud500 <christian.heinisch@protonmail.com>
2026-04-26 15:17:48 +0200

    chore(committees): register signal handlers in app config
    - import signals module in ready method

--- apps/committees/apps.py
+++ apps/committees/apps.py
@@ -9,3 +9,7 @@ class CommitteesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.committees'
     verbose_name = 'Gremienverwaltung'
+    
+    def ready(self):
+        """Import signal handlers when app is ready."""
+        import apps.committees.signals  # noqa: F401
